526b2b26a03a1edfd7d5f883221854e31a8c9e95
Jessy SOBREIRO <jessy.sobreiro@gmail.com>
2020-11-04 01:23:36 +0100

    Adding install script for DMRBridgeMQTT

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+## Check if we have sudo rights
+if [ "$EUID" -ne 0 ]
+  then echo "This script must be run as root. Exiting"
+  exit
+fi
+
+## Check if node is installed
+if command -v python3 > /dev/null
+  then true
+  else
+    echo "Package node is not installed. Exiting"
+    exit
+fi
+
+## Prepare a clean directory in /opt with needed files
+mkdir /opt/DMRBridgeWAV
+cp . /opt/DMRBridgeWAV -R
